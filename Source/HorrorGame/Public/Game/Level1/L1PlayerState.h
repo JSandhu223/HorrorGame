46b9198bbda1fec839878ed9dfc738fb61211e82
Jasraj Sandhu <jasrajsandhu01@gmail.com>
2025-08-24 17:36:02 -0600

    created player state class and set is as the player state in our game mode class

---
+++ Source/HorrorGame/Public/Game/Level1/L1PlayerState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "L1PlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORRORGAME_API AL1PlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
