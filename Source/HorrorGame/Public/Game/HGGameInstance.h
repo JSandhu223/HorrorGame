f631d54c7136745bcf74ade4938b467ea382dce7
Jasraj Sandhu <jasrajsandhu01@gmail.com>
2025-08-24 17:20:57 -0600

    Created game instance class. Disabled live coding.

---
+++ Source/HorrorGame/Public/Game/HGGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "HGGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORRORGAME_API UHGGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
