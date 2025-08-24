31cc2cb5910d90dcf508c218535c552740040806
Jasraj Sandhu <jasrajsandhu01@gmail.com>
2025-08-24 17:31:57 -0600

    created game state class and set it as the game state in our game mode class

---
+++ Source/HorrorGame/Public/Game/Level1/L1GameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "L1GameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORRORGAME_API AL1GameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
