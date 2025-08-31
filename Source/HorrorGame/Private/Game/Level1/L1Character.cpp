d5caff8c3f7ab2cb616c39934331dc1c3deb060a
Jasraj Sandhu <jasrajsandhu01@gmail.com>
2025-08-30 20:40:45 -0600

    explicitly set the character to use the controller rotation yaw

--- Source/HorrorGame/Private/Game/Level1/L1Character.cpp
+++ Source/HorrorGame/Private/Game/Level1/L1Character.cpp
@@ -12,6 +12,7 @@ AL1Character::AL1Character()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	bUseControllerRotationYaw = true;
 	bUseControllerRotationPitch = true;
 
 	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
