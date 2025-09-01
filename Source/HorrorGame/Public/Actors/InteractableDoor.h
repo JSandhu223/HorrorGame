17ec9e97999c9c5bca6f12529a96a85753f5f85d
Jasraj Sandhu <jasrajsandhu01@gmail.com>
2025-08-31 18:11:46 -0600

    created InteractableDoor c++ class as a child of the base class InteractableActor and derived a blueprint from it

---
+++ Source/HorrorGame/Public/Actors/InteractableDoor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/InteractableActor.h"
+#include "InteractableDoor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORRORGAME_API AInteractableDoor : public AInteractableActor
+{
+	GENERATED_BODY()
+	
+};
