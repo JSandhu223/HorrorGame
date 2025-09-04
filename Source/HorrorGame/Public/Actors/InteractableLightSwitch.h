7e884fffafaf378113c9109453e68b64d508bb48
Jasraj Sandhu <jasrajsandhu01@gmail.com>
2025-09-03 21:10:07 -0600

    created an interactable light switch based on the InteractableActor base class

---
+++ Source/HorrorGame/Public/Actors/InteractableLightSwitch.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/InteractableActor.h"
+#include "InteractableLightSwitch.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORRORGAME_API AInteractableLightSwitch : public AInteractableActor
+{
+	GENERATED_BODY()
+	
+};
