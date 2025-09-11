1ae4382a0da2f24517ec5d16c63f137999603eff
Jasraj Sandhu <jasrajsandhu01@gmail.com>
2025-09-10 20:27:07 -0600

    Created c++ class for inventory items. This is the base class all of our inventory items will be based off.

---
+++ Source/HorrorGame/Public/Actors/Inventory/InventoryItem.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Structs/ItemData.h"
+#include "InventoryItem.generated.h"
+
+UCLASS()
+class HORRORGAME_API AInventoryItem : public AActor
+{
+	GENERATED_BODY()
+
+private:
+	FItemData ItemData;
+	
+public:	
+	// Sets default values for this actor's properties
+	AInventoryItem();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
