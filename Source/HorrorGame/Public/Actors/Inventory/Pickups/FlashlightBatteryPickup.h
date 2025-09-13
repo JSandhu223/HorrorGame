525c44890479352a9a3e980da75771460d6e5631
Jasraj Sandhu <jasrajsandhu01@gmail.com>
2025-09-12 23:28:01 -0600

    created c++ and blueprint pickup actor for flashlight battery

---
+++ Source/HorrorGame/Public/Actors/Inventory/Pickups/FlashlightBatteryPickup.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/Inventory/Pickups/Pickup.h"
+#include "FlashlightBatteryPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORRORGAME_API AFlashlightBatteryPickup : public APickup
+{
+	GENERATED_BODY()
+	
+};
