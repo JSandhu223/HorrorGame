de608b78dba6756dd382c57f3aca012dc974af16
Jasraj Sandhu <jasrajsandhu01@gmail.com>
2025-10-04 10:53:44 -0600

    created pickup actor for doorkey item

---
+++ Source/HorrorGame/Private/Actors/Inventory/Pickups/DoorKeyPickup.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/Inventory/Pickups/DoorKeyPickup.h"
+
+
+ADoorKeyPickup::ADoorKeyPickup()
+{
+	this->Amount = 1;
+}
---
+++ Source/HorrorGame/Public/Actors/Inventory/Pickups/DoorKeyPickup.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/Inventory/Pickups/Pickup.h"
+#include "DoorKeyPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORRORGAME_API ADoorKeyPickup : public APickup
+{
+	GENERATED_BODY()
+	
+
+public:
+	ADoorKeyPickup();
+};
